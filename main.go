84fa95a732179f5d65ed70063757bc10fbb34224
virtouso <s_moeen_kamali@yahoo.com>
2024-01-19 13:25:48 +0330

    getting the input from the user about philosophers count

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"github.com/virtouso/DiningPhilosophersGolang-/models"
+	"strconv"
+)
+
+var userInput string
+var philosophersCount int
+var inputError error
+
+var philosophers []models.Philosopher
+var chopsticks []models.Chopstick
+
 func main() {
-	println("initial test of running the project....")
+
+	for philosophersCount <= 1 {
+		fmt.Println("write number of dining philosophers (any integer value):")
+		fmt.Scanln(&userInput)
+		philosophersCount, inputError = strconv.Atoi(userInput)
+		if inputError != nil {
+			fmt.Println("invalid input. try again.")
+		}
+	}
+
+	println("starting the infinite eat process")
+
+	philosophers = make([]models.Philosopher, philosophersCount)
+	chopsticks = make([]models.Chopstick, philosophersCount)
+
 }
